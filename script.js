becc35374c1c7f69d4213afa475d954275eb67ef
lvmendess <liviamendes1703@gmail.com>
2024-11-02 17:12:39 -0300

    xccxcxvvb mnmjnjhmnhggffgfdfdfghgjhkjjkkj

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const products = [
-    {id: 'promo1', name: 'Camiseta Brasão Branca', image: 'resources/peças/camiseta-brasao.svg'},
-    {id: 'promo2', name: 'Camiseta Cavalo', image: 'resources/peças/camiseta-cavalo.svg'},
-    {id: 'promo3', name: 'Camiseta Cruz', image: 'resources/peças/camiseta-cruz.svg'},
-    {id: 'promo4', name: 'Camiseta Los Angeles', image: 'resources/peças/camiseta-losangeles.svg'},
-    {id: 'cam1', name: 'Camiseta Silkada', image: 'resources/peças/camiseta-sambademinas.svg'},
-    {id: 'cam2', name: 'Camisa Pólo Piquet Bordada', image: 'resources/peças/camiseta-esata.svg'},
-    {id: 'cam3', name: 'camiseta 3', image: 'resources/peças/camiseta-netview.svg'},
-    {id: 'cam4', name: 'camiseta 4', image: 'resources/blusa.jpg'}
+    {id: 'promo1', name: 'Camiseta Estampa Brasão Branca', image: 'resources/pecas/camiseta-brasao.svg'},
+    {id: 'promo2', name: 'Camiseta Estampa Cavalo', image: 'resources/pecas/camiseta-cavalo.svg'},
+    {id: 'promo3', name: 'Camiseta Estampa Cruz', image: 'resources/pecas/camiseta-cruz.svg'},
+    {id: 'promo4', name: 'Camiseta Estampa Los Angeles', image: 'resources/pecas/camiseta-losangeles.svg'},
+    {id: 'cam1', name: 'Camiseta Estampa Avião', image: 'resources/pecas/camiseta-aviao.svg'},
+    {id: 'cam2', name: 'Camiseta Estampa Fusca', image: 'resources/pecas/camiseta-fusca.svg'},
+    {id: 'cam3', name: 'Camiseta Estampa Espada', image: 'resources/pecas/camisetaEspada-frenteEcostas.svg'},
+    {id: 'cam4', name: 'Camiseta Estampa Fusca', image: 'resources/pecas/camiseta-fusca.png'}
     /*{id: 'cal1', name: 'calça 1', image: 'resources/shirt.jpg'},
     {id: 'cal2', name: 'calça 2', image: 'resources/blusa.jpg'},
     {id: 'cal3', name: 'calça 3', image: 'resources/blusa.jpg'},
